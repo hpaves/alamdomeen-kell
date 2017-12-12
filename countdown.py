0e66ef54b4fdecc2cd384fb25c3c6ce8141e61f5
hpaves <henri.paves@gmail.com>
2017-12-12 18:03:42 +0200

    printing the actual time difference finally works

--- countdown.py
+++ countdown.py
@@ -1,8 +1,12 @@
+import time
 import datetime
 
-dt = datetime.datetime(2018, 12, 13)
-now = datetime.datetime.now()
-
-delta = dt - now
-
-print delta
+while True:
+    rawLessonTime = "18:30:00.000000"
+    rawCurrentTime = datetime.datetime.now().time()
+    FMT = "%H:%M:%S.%f" # idea from here: https://stackoverflow.com/questions/3096953/how-to-calculate-the-time-interval-between-two-time-strings
+    lessonTime = datetime.datetime.strptime(rawLessonTime, FMT)
+    currentTime = datetime.datetime.strptime(str(rawCurrentTime), FMT)
+    delta = lessonTime - currentTime
+    print str(delta).split(".")[0]
+    time.sleep(1)
