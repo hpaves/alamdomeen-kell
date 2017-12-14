1d7ae6ce5aa30a8838bd9e5aa236e454e94b121a
hpaves <henri.paves@gmail.com>
2017-12-14 17:14:02 +0200

    added main method, fixed invalid variable and constant names

--- countdown.py
+++ countdown.py
@@ -1,16 +1,20 @@
 import time
 import datetime
 
-while True:
-    rawLessonTime = "8:30:00.000000"
-    rawCurrentTime = datetime.datetime.now().time() # get current time, strip the date
-    FMT = "%H:%M:%S.%f" # idea from here: https://stackoverflow.com/questions/3096953/how-to-calculate-the-time-interval-between-two-time-strings
-    lessonTime = datetime.datetime.strptime(rawLessonTime, FMT) # make string back to datetime
-    currentTime = datetime.datetime.strptime(str(rawCurrentTime), FMT) # convert stripped time to string, and then back to datetime
-    if lessonTime < currentTime:
-        lessonTime = lessonTime + datetime.timedelta(days=1) # add one day to the lessonTime if the lessons are over for today
-    else:
-        pass
-    delta = lessonTime - currentTime
-    print str(delta).split(".")[0]
-    time.sleep(1)
+def main():
+    while True:
+        raw_lesson_time = "8:30:00.000000"
+        raw_current_time = datetime.datetime.now().time() # get current time, strip the date
+        time_formatter_formula = "%H:%M:%S.%f" # idea from here: http://bit.ly/2C3F0Rj
+        lesson_time = datetime.datetime.strptime(raw_lesson_time, time_formatter_formula)
+        current_time = datetime.datetime.strptime(str(raw_current_time), time_formatter_formula)
+        if lesson_time < current_time: # add one day if the lessons are over for today
+            lesson_time = lesson_time + datetime.timedelta(days=1)
+        else:
+            pass
+        delta = lesson_time - current_time
+        print str(delta).split(".")[0]
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
