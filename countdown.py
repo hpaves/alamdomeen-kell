0746cdd42ac6f743a87e0e1542acff01134ff74c
hpaves <henri.paves@gmail.com>
2017-12-14 23:23:11 +0200

    script picks the correct file, depending on the present weekday

--- countdown.py
+++ countdown.py
@@ -9,16 +9,22 @@ def main():
     """This it the main function of the script that executes other functions."""
     countdown()
 
+def pick_the_correct_file():
+    """If monday, picks timetable1, if any other weekday, picks timetable2."""
+    if datetime.datetime.today().weekday() == 0:
+        return "timetable1"
+    else:
+        return "timetable2"
 
-def readfile():
+def read_file():
     """Reads a timetable from a file and makes it a list."""
-    with open("timetable1") as filecontents:
-        timetable_list = filecontents.read().splitlines()
+    with open(pick_the_correct_file()) as file_contents:
+        timetable_list = file_contents.read().splitlines()
         return timetable_list
 
-def findnextlesson():
+def find_next_lesson():
     """Iterates over the timetable to find out the start of the next lesson."""
-    timetable_today = readfile()
+    timetable_today = read_file()
     counter = 0
     for specific_time in timetable_today:
         lesson_time_string = specific_time + str(".000000")
@@ -36,7 +42,7 @@ def findnextlesson():
 def countdown():
     """"Compares the school bell time to the current time."""
     while True:
-        lesson_time_string = findnextlesson() + str(".000000")
+        lesson_time_string = find_next_lesson() + str(".000000")
         current_time_string = datetime.datetime.now().time() # get current time, strip the date
         time_formatter_formula = "%H:%M:%S.%f" # idea from here: http://bit.ly/2C3F0Rj
         lesson_time = datetime.datetime.strptime(lesson_time_string, time_formatter_formula)
@@ -48,10 +54,6 @@ def countdown():
         delta = lesson_time - current_time
         print(str(delta).split(".")[0])
         time.sleep(1)
-
-def checkday():
-    """If monday, picks timetable1, if any other weekday, picks timetable2,
-    if weekend, displays a message that tells people to go home."""
 
 if __name__ == "__main__":
     main()
