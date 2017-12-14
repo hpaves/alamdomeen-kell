c1df9f2ccd688757d9d73378f4dd816af3e9311a
hpaves <henri.paves@gmail.com>
2017-12-14 22:14:29 +0200

    script reads from the file and compares with the next lesson, but optimizing needed

--- countdown.py
+++ countdown.py
@@ -7,23 +7,40 @@ import datetime
 
 def main():
     """This it the main function of the script that executes other functions."""
-#    comparetime()
-    readfile()
+    countdown()
+
 
 def readfile():
     """Reads a timetable from a file and makes it a list."""
     with open("timetable1") as filecontents:
         timetable_list = filecontents.read().splitlines()
-        print timetable_list
+        return timetable_list
 
-def comparetime():
+def findnextlesson():
+    """Iterates over the timetable to find out the start of the next lesson."""
+    timetable_today = readfile()
+    counter = 0
+    for specific_time in timetable_today:
+        lesson_time_string = specific_time + str(".000000")
+        current_time_string = datetime.datetime.now().time() # get current time, strip the date
+        time_formatter_formula = "%H:%M:%S.%f" # idea from here: http://bit.ly/2C3F0Rj
+        lesson_time = datetime.datetime.strptime(lesson_time_string, time_formatter_formula)
+        current_time = datetime.datetime.strptime(str(current_time_string), time_formatter_formula)
+        if lesson_time > current_time:
+            return str(lesson_time).split(" ")[1]
+        else:
+            counter = counter + 1
+            if counter == len(timetable_today):
+                return timetable_today[0]
+
+def countdown():
     """"Compares the school bell time to the current time."""
     while True:
-        raw_lesson_time = "8:30:00.000000"
-        raw_current_time = datetime.datetime.now().time() # get current time, strip the date
+        lesson_time_string = findnextlesson() + str(".000000")
+        current_time_string = datetime.datetime.now().time() # get current time, strip the date
         time_formatter_formula = "%H:%M:%S.%f" # idea from here: http://bit.ly/2C3F0Rj
-        lesson_time = datetime.datetime.strptime(raw_lesson_time, time_formatter_formula)
-        current_time = datetime.datetime.strptime(str(raw_current_time), time_formatter_formula)
+        lesson_time = datetime.datetime.strptime(lesson_time_string, time_formatter_formula)
+        current_time = datetime.datetime.strptime(str(current_time_string), time_formatter_formula)
         if lesson_time < current_time: # +1 day to avoid negative if the lessons are over for today
             lesson_time = lesson_time + datetime.timedelta(days=1)
         else:
